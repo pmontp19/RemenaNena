366584337845f8e4c6ac222e5e37f2cce243a609
victo <victo@DESKTOP-MPDJG3Q>
2019-05-08 17:44:31 +0200

    inicio

---
+++ src/remena_nena/domini/Campanya.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package remena_nena.domini;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author victo
+ */
+public class Campanya {
+    private String nom;
+    private boolean estat;
+    private ArrayList<Etapa> e_forma;
+    private ArrayList<Etapa> e_iniciades;
+    private Etapa e_inicial;
+    
+    
+}
